2e9f34afba1e73ae72c5eeead158985fc3799bba
Darwin Dela Cruz <darwindelacruz0012@gmail.com>
2026-03-02 21:46:49 +0800

    Create startup logic

--- src/sql_blocker/main.py
+++ src/sql_blocker/main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+import tkinter as tk
+from .gui import BlockerMonitorApp
+from .logger import log_message
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def main():
+    root = tk.Tk()
+    app = BlockerMonitorApp(root)
+    log_message(app, "SQL Blocker Monitor Initiated.")
+    root.mainloop()git
